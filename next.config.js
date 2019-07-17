628cb424e698da4bcdcc4ed0256e47b0e13975b8
Mariano Fliri <mfliri@as-one.com.ar>
2019-07-17 19:28:24 -0300

    update next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const withSass = require('@zeit/next-sass')
+const isProd = (process.env.NODE_ENV || 'production') === 'production'
 
 module.exports =withSass({
   exportPathMap: function () {
@@ -6,6 +7,7 @@ module.exports =withSass({
       '/': { page: '/' },
     }
   },
+  assetPrefix: !isProd ? '/iframeInfoAuto/' : '',
   webpack: (config) => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
