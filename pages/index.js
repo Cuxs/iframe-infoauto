08cfd4578f6b2cee17ace65c02ec2a5da421e902
Mariano Fliri <mfliri@as-one.com.ar>
2019-07-17 18:31:33 -0300

    initial Commit

---
+++ pages/index.js
@@ -0,0 +1,60 @@
+import '../sass/main.scss';
+import makeAnimated from 'react-select/animated';
+import Select from 'react-select'
+
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' }
+]
+
+const customStyles = {
+  option: (provided, state) => ({
+    ...provided,
+    borderBottom: '1px dotted pink',
+    color: state.isSelected ? 'red' : 'blue',
+    padding: 20,
+  }),
+  control: () => ({
+    // none of react-select's styles are passed to <Control />
+    width: 200,
+  }),
+  singleValue: (provided, state) => {
+    const opacity = state.isDisabled ? 0.5 : 1;
+    const transition = 'opacity 300ms';
+
+    return { ...provided, opacity, transition };
+  }
+}
+
+
+export default class FlavorForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 'coconut'};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Your favorite flavor is: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Completa los datos para obtener el precio del vehículo
+          <Select components={makeAnimated()} options={options} styles={customStyles} />
+        </label>
+        <button type="submit" className="btn-primary" value="Submit">Ver precio</button>
+      </form>
+    );
+  }
+}
